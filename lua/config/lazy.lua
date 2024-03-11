4f626c1b9704168b1cf47bbb581931ca2e569e72
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2024-03-11 06:29:37 +0100

    Feat: Added wakatime, code-snap, vim-devicons

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -7,46 +7,50 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+   spec = {
+      -- add LazyVim and import its plugins
+      { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
 
-      colorscheme = "sonokai",
-    } },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+         colorscheme = "sonokai",
+      } },
+
+      { "wakatime/vim-wakatime", lazy = false },
+      { "mistricky/codesnap.nvim", build = "make" },
+      { "ryanoasis/vim-devicons", lazy = false },
+      { "nvim-tree/nvim-web-devicons", lazy = false },
+      -- import any extras modules here
+      { import = "lazyvim.plugins.extras.linting.eslint" },
+      { import = "lazyvim.plugins.extras.formatting.prettier" },
+      { import = "lazyvim.plugins.extras.lang.typescript" },
+      { import = "lazyvim.plugins.extras.lang.json" },
+      { import = "lazyvim.plugins.extras.coding.copilot" },
+      { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+      { import = "lazyvim.plugins.extras.lang.tailwind" },
+      { import = "plugins" },
+   },
+   defaults = {
+      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+      lazy = false,
+      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+      -- have outdated releases, which may break your Neovim install.
+      version = false, -- always use the latest git commit
+      -- version = "*", -- try installing the latest stable version for plugins that support semver
+   },
+   install = { colorscheme = { "tokyonight", "habamax" } },
+   checker = { enabled = true }, -- automatically check for plugin updates
+   performance = {
+      rtp = {
+         -- disable some rtp plugins
+         disabled_plugins = {
+            -- "matchit",
+            -- "matchparen",
+            -- "netrwPlugin",
+            "tarPlugin",
+            "tohtml",
+            "tutor",
+            "zipPlugin",
+         },
       },
-    },
-  },
+   },
 })
